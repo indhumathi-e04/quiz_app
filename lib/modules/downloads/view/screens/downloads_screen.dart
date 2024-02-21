5dd2d69f275a80008eb32d0aaf58c0cc778adc42
Manimaran K V <kingmanimarankv@gmail.com>
2024-02-21 15:15:18 +0530

    Fix UI in download screen

--- lib/modules/downloads/view/screens/downloads_screen.dart
+++ lib/modules/downloads/view/screens/downloads_screen.dart
@@ -3,6 +3,8 @@ import 'package:get/get.dart';
 
 import '../../../../constants/ui_constants.dart';
 import '../../../common/view/widgets/custom_widgets/custom_elevated_button.dart';
+import '../../../common/view/widgets/name_badge.dart';
+import '../../../practice/view/screens/practice_screen.dart';
 import '../view_model/downloads_controller.dart';
 
 class DownloadsScreen extends StatelessWidget {
@@ -16,71 +18,76 @@ class DownloadsScreen extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(
-          UIConstants.defaultPadding,
+          UIConstants.defaultPadding * 2,
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Your Downloads",
-              style: Theme.of(context).textTheme.bodyMedium,
-            ),
-            const SizedBox(
-              height: UIConstants.defaultHeight * 0.5,
-            ),
-            Text(
+        child: Section(
+          title: "Your Downloads",
+          subtitle:
               "Here's a list of all your downloads. You can utilize the downloaded tests to take your tests offline.",
-              style: Theme.of(context).textTheme.labelSmall,
-            ),
-            const SizedBox(
-              height: UIConstants.defaultHeight * 0.5,
-            ),
-            Wrap(
-              // list of length 3
-              children: List.generate(
-                3,
-                (int index) {
-                  return Obx(
-                    () => Container(
-                      margin: const EdgeInsets.only(
-                        right: UIConstants.defaultMargin,
-                      ),
-                      child: ChoiceChip(
-                        padding: const EdgeInsets.all(
-                          UIConstants.defaultPadding,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Wrap(
+                children: List.generate(
+                  3,
+                  (int index) {
+                    return Obx(
+                      () => Container(
+                        margin: const EdgeInsets.only(
+                          right: UIConstants.defaultMargin,
                         ),
-                        label: Text(controller.choicechipsLabelText[index]),
-                        selectedColor: Colors.grey,
-                        selected: controller.value.value == index,
-                        onSelected: (bool selected) {
-                          controller.value.value = selected ? index : null;
-                        },
+                        child: ChoiceChip(
+                          padding: const EdgeInsets.all(
+                            UIConstants.defaultPadding * 0.5,
+                          ),
+                          label: Text(
+                            controller.choicechipsLabelText[index],
+                          ),
+                          labelStyle: Theme.of(context).textTheme.bodySmall,
+                          backgroundColor: Theme.of(context)
+                              .colorScheme
+                              .surface
+                              .withOpacity(0.05),
+                          selectedColor:
+                              Theme.of(context).colorScheme.secondary,
+                          selected: controller.value.value == index,
+                          onSelected: (bool selected) {
+                            controller.value.value = selected ? index : null;
+                          },
+                        ),
                       ),
+                    );
+                  },
+                ).toList(),
+              ),
+              const SizedBox(
+                height: UIConstants.defaultHeight * 0.5,
+              ),
+              ListView.separated(
+                separatorBuilder: (context, index) {
+                  return const SizedBox(
+                    height: UIConstants.defaultHeight,
+                  );
+                },
+                shrinkWrap: true,
+                itemCount: 3,
+                itemBuilder: (BuildContext context, index) {
+                  return Downloads(
+                    downloadedDate: "4th March 2024",
+                    testDetails: TestDetailsModel(
+                      testTitle: "NEET - 2023 Full Length Mock Test",
+                      creatorName: "Manimaran K V",
+                      isVerified: true,
+                      totalQuestions: 10,
+                      timeLimit: 90,
+                      attemptsCount: 1500,
+                      coins: 10,
                     ),
                   );
                 },
-              ).toList(),
-            ),
-            const SizedBox(
-              height: UIConstants.defaultHeight * 0.5,
-            ),
-            ListView.separated(
-              separatorBuilder: (context, index) {
-                return const SizedBox(
-                  height: UIConstants.defaultHeight,
-                );
-              },
-              shrinkWrap: true,
-              itemCount: 3,
-              itemBuilder: (BuildContext context, index) {
-                return const Downloads(
-                  downloadedDate: "4th March 2024",
-                  examTitle: 'NEET - 2024 Full Length Mock Test',
-                  examName: 'Manimaran K V',
-                );
-              },
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -89,13 +96,11 @@ class DownloadsScreen extends StatelessWidget {
 
 class Downloads extends StatelessWidget {
   const Downloads({
-    required this.examTitle,
-    required this.examName,
+    required this.testDetails,
     required this.downloadedDate,
     super.key,
   });
-  final String examTitle;
-  final String examName;
+  final TestDetailsModel testDetails;
   final String downloadedDate;
 
   @override
@@ -114,60 +119,31 @@ class Downloads extends StatelessWidget {
         ),
       ),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                examTitle,
-                style: Theme.of(context).textTheme.labelLarge,
-              ),
-              Row(
-                children: [
-                  Icon(
-                    Icons.delete,
-                    size: UIConstants.defaultHeight * 1.2,
-                    color: Theme.of(context).colorScheme.primary,
-                  ),
-                  const SizedBox(
-                    width: UIConstants.defaultHeight * 0.5,
-                  ),
-                  Icon(
-                    Icons.refresh,
-                    size: UIConstants.defaultHeight * 1.2,
-                    color: Theme.of(context).colorScheme.primary,
-                  ),
-                ],
-              )
-            ],
+          Text(
+            testDetails.testTitle ?? "",
+            style: Theme.of(context).textTheme.headlineSmall,
           ),
-          Row(
-            children: [
-              Text(
-                examName,
-                style: Theme.of(context).textTheme.labelSmall,
-              ),
-              const SizedBox(
-                width: UIConstants.defaultWidth * 0.5,
-              ),
-              Icon(
-                Icons.verified,
-                size: UIConstants.defaultHeight * 1.2,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-            ],
+          const SizedBox(
+            height: UIConstants.defaultHeight * 0.5,
+          ),
+          NameBadge(
+            name: testDetails.creatorName ?? "",
+            isVerifiedUser: testDetails.isVerified ?? false,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               Text(
                 "Downloaded at $downloadedDate",
-                style: Theme.of(context).textTheme.labelSmall,
+                style: Theme.of(context).textTheme.bodySmall,
               ),
               CustomElevatedButton(
-                buttonWidth: 60,
-                buttonHeight: 20,
+                buttonHeight: UIConstants.defaultHeight * 2.5,
+                buttonWidth: UIConstants.defaultWidth * 8,
                 buttonText: "Attempt",
                 onPressed: () {},
                 isLoading: false,
